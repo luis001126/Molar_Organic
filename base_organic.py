d9f380247b0c0ea31b0f7b0120c6e988cc2948d1
luis001126 <luisalexisrojasrondan@gmail.com>
2023-11-09 18:09:44 -0600

    5 commit
    desarrollo del menu

--- base_organic.py
+++ base_organic.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import StringVar
 from tkinter import PhotoImage
 from tkinter import messagebox as mBox
+from tkinter import Menu
+
 C = 12
 H = 1
 O = 16
@@ -44,65 +46,77 @@ root.resizable(0, 0)
 
 imagen =PhotoImage(file='molar.png')
 Label(root,image=imagen,bd=12).pack()
+#------------------------------------------
+barra_menu = Menu(root)
+root.config(menu=barra_menu)
 
+funcionamiento = Menu(barra_menu,tearoff=0)
+funcionamiento.add_command(label = 'Funcionamiento')
+funcionamiento.add_separator()
+funcionamiento.add_command(label='Ayuda')
+funcionamiento.add_separator()
+funcionamiento.add_command(label='Referencias')
+barra_menu.add_cascade(label = 'Acerca de: ',menu=funcionamiento)
+
+#------------------------------------------
 etiqueta = StringVar()
 etiqueta = Label(root, text='Seleccione las sustancia quimica a calcular')
-etiqueta.pack()
+etiqueta.place(x=0,y=40)
 
 opcion1 = IntVar()
 radio1 = Radiobutton(root,text='Carbono-Hidrogeno',variable=opcion1,value=1, command=Funcion)
-radio1.place(x=0,y=20)
+radio1.place(x=0,y=20+40)
 radio2 = Radiobutton(root,text='Carbono-Hidrogeno-Oxigeno',variable=opcion1,value=2, command=Funcion)
-radio2.place(x=0,y=40)
+radio2.place(x=0,y=40+40)
 radio3 = Radiobutton(root,text='Carbono-Hidrogeno-Oxigeno-Nitrogeno',variable=opcion1,value=3, command=Funcion)
-radio3.place(x=0,y=60)
+radio3.place(x=0,y=60+40)
 radio4 = Radiobutton(root,text='Carbono-Hidrogeno-Oxigeno-Nitrogeno-Azufre',variable=opcion1,value=4, command=Funcion)
-radio4.place(x=0,y=80)
+radio4.place(x=0,y=80+40)
 #----------------------------------------
 titulo1=StringVar()
 titulo1=Label(root,text='Ingrese la cantidad de elementos:')
-titulo1.place(x=0,y=120)
+titulo1.place(x=0,y=120+40)
 
 #----------------------------------------
 etiqueta2 = StringVar()
 etiqueta2 = Label(root,text='Carbono',justify=CENTER)
-etiqueta2.place(x=0,y=150)
+etiqueta2.place(x=0,y=150+40)
 entrada1= IntVar()
 entrada1.set('')
 Cabro = Entry(root,textvariable=entrada1,justify=CENTER)
-Cabro.place(x=0,y=175)
+Cabro.place(x=0,y=175+40)
 #----------------------------------------
 etiqueta3 = StringVar()
 etiqueta3 = Label(root,text='Hidogeno',justify=CENTER)
-etiqueta3.place(x=0,y=200)
+etiqueta3.place(x=0,y=200+40)
 entrada2 =IntVar()
 entrada2.set('')
 Hidro = Entry(root,textvariable=entrada2,justify=CENTER)
-Hidro.place(x=0,y=225)
+Hidro.place(x=0,y=225+40)
 #---------------------------------------
 etiqueta4 = StringVar()
 etiqueta4 = Label(root,text='Oxigeno',justify=CENTER)
-etiqueta4.place(x=200,y=150)
+etiqueta4.place(x=200,y=150+40)
 entrada3 =IntVar()
 entrada3.set('')
 Oxi = Entry(root,textvariable=entrada3,justify=CENTER)
-Oxi.place(x=200,y=175)
+Oxi.place(x=200,y=175+40)
 #--------------------------------------
 etiqueta5 = StringVar()
 etiqueta5 = Label(root,text='Nitrogeno',justify=CENTER)
-etiqueta5.place(x=200,y=200)
+etiqueta5.place(x=200,y=200+40)
 entrada4 =IntVar()
 entrada4.set('')
 Nitro = Entry(root,textvariable=entrada4,justify=CENTER)
-Nitro.place(x=200,y=225)
+Nitro.place(x=200,y=225+40)
 #-------------------------------------
 etiqueta6 = StringVar()
 etiqueta6 = Label(root,text='Azufre',justify=CENTER)
-etiqueta6.place(x=0,y=250)
+etiqueta6.place(x=0,y=250+40)
 entrada5 =IntVar()
 entrada5.set('')
 Azuf = Entry(root,textvariable=entrada5,justify=CENTER)
-Azuf.place(x=0,y=275)
+Azuf.place(x=0,y=275+40)
 #-------------------------------------
 boton1 = Button(root,text='SALIR',command=Salir)
 boton1.place(x=350,y=360)
